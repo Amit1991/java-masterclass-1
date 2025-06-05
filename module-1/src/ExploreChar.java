213b887ad67e39766ec22a22c0da0e70fb4fa8aa
Amit Kumar <amit.bit.it@gmail.com>
2025-06-05 22:46:31 +0530

    Commit 8: ExploreChar is created in the same fashion of ExploreFloat, ExploreLong, ExploreByte, ExploreInteger and ExploreShort.

---
+++ module-1/src/ExploreChar.java
@@ -0,0 +1,12 @@
+public class ExploreChar {
+
+    public static void main(String[] args) {
+
+        char c = '\u03BB';
+        char c1 = 'f';
+        char c2 = 89;
+        System.out.println(Character.toLowerCase(c2));
+        System.out.println(Character.toUpperCase(c2));
+        System.out.println(c1 + " / " + c);
+    }
+}
